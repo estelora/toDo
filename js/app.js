665681db98be2d38b60c41f8578f43df2eacda2c
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-06-21 14:46:53 -0700

    create ItemController

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
 (function() {
+
   var app = angular.module('list', [ ]);
+
+  app.controller("ItemController", function(){
+
+      this.item= {};
+
+      this.addItem = function(item){
+        list.item.push(this.item);
+        this.item = {};
+      };
+    });
+
 })();
