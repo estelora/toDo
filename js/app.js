d88a27e748d1f04c656baa3603a4e271026c1203
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-06-22 16:59:00 -0700

    added 404 hanlder

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
-var toDo = angular.module("toDo", []);
+var toDo = angular.module("toDo", ['ngRoute']);
+
+//Create our 404 handler
+toDo.config(['$routeProvider', function ($routeProvider) {
+  $routeProvider.otherwise({
+    controller: 'Error404Ctrl',
+    controllerAs: 'vm',
+    templateUrl: 'errors/404/error-404.html'
+  });
+}]);
+
 toDo.factory("items", function () {
     var items = {};
     items.data = [{
