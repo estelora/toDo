b63fa4c045af9964b20fdddb8cf2b4553fdfe304
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-06-22 17:13:51 -0700

    create item-detail.html

---
+++ js/items.js
@@ -0,0 +1,13 @@
+app.config(['$routeProvider', function ($routeProvider) {
+  //object to define the route
+  // var routeDefinition = {
+  //   controller: 'ItemCtrl',
+  //   controllerAs: 'itemDetail',
+  //   templateUrl: 'item-detail.html'
+  };
+
+  //angular parses that var here looks something like below
+  //$routeProvider.when('/', routeDefinition);
+  // $routeProvider.when('/item-detail', routeDefinition);
+}]);
+
