4d61c15be5c67786b16fc06cc2a8c8349a55d46d
Lydia E. Joslin <lejoslin@ncsu.edu>
2015-06-23 11:05:03 -0700

    added link for ngRoute to index.html

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 var toDo = angular.module('toDo', ['ngRoute']);
 
-//Create our 404 handler
-toDo.config(['$routeProvider', function ($routeProvider) {
-  $routeProvider.otherwise({
-    controller: 'Error404Ctrl',
-    controllerAs: 'vm',
-    templateUrl: 'errors/404/error-404.html'
-  });
-}]);
+// //Create our 404 handler
+// toDo.config(['$routeProvider', function ($routeProvider) {
+//   $routeProvider.otherwise({
+//     controller: 'Error404Ctrl',
+//     controllerAs: 'vm',
+//     templateUrl: 'errors/404/error-404.html'
+//   });
+// }]);
 
